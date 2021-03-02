36981d29b394232873497f31ca7887101e3c06a2
Bruno Ruiz de Somocurcio <44142527+BrunoRDS@users.noreply.github.com>
2021-03-02 09:30:40 -0800

    Morning Comit

---
+++ Address_Design/Controllers/AddressFormController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Address_Design.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressFormController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+    }
+}

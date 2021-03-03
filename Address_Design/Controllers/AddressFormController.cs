5bce42827a5c8a7e9b192ec02727a1c2b88f1c0c
Sam Borhan <samborhan1566@gmail.com>
2021-03-03 09:38:49 -0800

    Link  Address From Controller and View

--- Address_Design/Controllers/AddressFormController.cs
+++ Address_Design/Controllers/AddressFormController.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Text.Encodings.Web;
+using System.Web;
+using Address_Design.Models;
 
 namespace Address_Design.Controllers
 {
@@ -13,6 +15,10 @@ namespace Address_Design.Controllers
     [ApiController]
     public class AddressFormController : Controller
     {
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
         private readonly ILogger<AddressFormController> _logger;
 
         public AddressFormController(ILogger<AddressFormController> logger)
@@ -20,55 +26,88 @@ namespace Address_Design.Controllers
             _logger = logger;
         }
 
-        //
-        // GET: /AddressForm/
+
+        //GET: /AddressForm/
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-
-
-        //
-        // GET: /AddressForm/FilledForm
-        [HttpGet("FilledForm")]
-        public IActionResult FilledForm(string name, int numTimes = 1)
+        [HttpPost]
+        public JsonResult PostAddress(Address incomingAddress)
         {
-            ViewData["Message"] = "Hello " + name;
-            ViewData["NumTimes"] = numTimes;
-
-            return View();
-        }
-
-
-        //
-        // GET: /AddressForm/Get1
-        [HttpGet("Get1")]
-        public string[] Get1()
-        {
-            return new string[]
+            Address status = null;
+            try
             {
-                "This is a stub for a second GET",
-                "REPLACE ME"
-            };
+                saveAddress(incomingAddress);
+                status = new Address() { Street = "main street", ZipCode = "9805" };
+            }
+            catch (Exception e)
+            {
+                new NotImplementedException();
+            }
+
+            return Json(status);
         }
 
-        //
-        // GET: /AddressForm/Get2?name=Rick&numtimes=4
-        [HttpGet("Get2")]
-        public string Get2(string name, int numTimes = 1)
+        #region privateHelpers
+        private Boolean saveAddress(Address incomingAddress)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            if (incomingAddress.Street is null)
+            {
+                // do something...
+                return false;
+            }
+            else
+            {
+                // do something positive!
+                return true;
+            }
         }
+        #endregion
+    }
 
-        //
-        // GET: /AddressForm/Get3/3?name=Rick
-        [HttpGet("Get3")]
-        public string Get3(string name, int ID = 1)
-        {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
-        }
+
+    ////
+    //// GET: /AddressForm/FilledForm
+    //[HttpGet("FilledForm")]
+    //    public IActionResult FilledForm(string name, int numTimes = 1)
+    //    {
+    //        ViewData["Message"] = "Hello " + name;
+    //        ViewData["NumTimes"] = numTimes;
+
+    //        return View();
+    //    }
+
+
+    //    //
+    //    // GET: /AddressForm/Get1
+    //    [HttpGet("Get1")]
+    //    public string[] Get1()
+    //    {
+    //        return new string[]
+    //        {
+    //            "This is a stub for a second GET",
+    //            "REPLACE ME"
+    //        };
+    //    }
+
+    //    //
+    //    // GET: /AddressForm/Get2?name=Rick&numtimes=4
+    //    [HttpGet("Get2")]
+    //    public string Get2(string name, int numTimes = 1)
+    //    {
+    //        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    //    }
+
+    //    //
+    //    // GET: /AddressForm/Get3/3?name=Rick
+    //    [HttpGet("Get3")]
+    //    public string Get3(string name, int ID = 1)
+    //    {
+    //        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    //    }
 
 
 
@@ -77,4 +116,5 @@ namespace Address_Design.Controllers
 
 
     }
-}
+;
+
---
+++ Address_Design/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Address_Design.Models
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        
+        public string ZipCode { get; set; }
+    }
+}

20f126d8d92b79fff8b88d79b60c8223bd746834
Ruifeng Wang <rwang@seattleu.edu>
2021-03-07 15:09:32 -0800

    Added all fields to Address model

--- Address_Design/Models/Address.cs
+++ Address_Design/Models/Address.cs
@@ -8,9 +8,15 @@ namespace Address_Design.Models
 {
     public class Address
     {
+        public string Country { get; set; }
+
         public string Recipient { get; set; }
-        
+
         public string AddressLine { get; set; }
+
+        public string Unit_Apt { get; set;}
+
+        public string PostalCode { get; set; }
 
         public string City { get; set; }
 
@@ -18,6 +24,49 @@ namespace Address_Design.Models
         
         public string ZipCode { get; set; }
 
-        public string Country { get; set; }
+        public string ProvinceCode { get; set; }
+
+        public string Province { get; set; }
+
+        public string County { get; set; }
+
+        public string District { get; set; }
+
+        public string Island { get; set; }
+
+        public string StreetSection { get; set; }
+
+        public string Canton { get; set; }
+
+        public string PostOfficeIdentifier { get; set; }
+
+        public string PostalDistrict { get; set; }
+
+        public string Locality { get; set; }
+
+        public string BuildingName { get; set; }
+
+        public string BuildingNumber { get; set; }
+
+        public string SubareaNumber { get; set; }
+
+        public string Subarea { get; set; }
+
+        public string Prefecture { get; set; }
+
+        public string Subdivision { get; set; }
+
+        public string Neighborhood { get; set; }
+
+        public string Suburb { get; set; }
+
+        public string BP { get; set; }
+
+        public string PostOffice { get; set; }
+
+        public string HouseNumber { get; set; }
+
+        public string ApartmentNumber { get; set; }
+
     }
 }
